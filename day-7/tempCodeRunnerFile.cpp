cf440f80639760350393e74a9eb7deb1b185e2cb
shiro-han <shan99business@gmail.com>
2020-12-08 20:51:42 -0500

    wrong answer

--- day-7/tempCodeRunnerFile.cpp
+++ day-7/tempCodeRunnerFile.cpp
@@ -1,14 +1,5 @@
-//if bag contains no other bags return false
-    // if (foundBag.rules.at(0).adj == keyword2){
-    //     return false;
-    // }
+ = foundBag.rules.at(0).adj;
 
-    // //check bag rules to see if shiny gold is an adj of one of its rules
-    // for (int i = 0; i < foundBag.rules.size(); i++){
-    //     string currAdj = foundBag.rules.at(i).adj;
-    //     if (currAdj == keyword0){
-    //         return true;
-    //     } else {
-    //         output = (output || checkBags(bags, currAdj));
-    //     }
+    // if (currAdj == keyword2){
+    //     return false;
     // }
